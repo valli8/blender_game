b4652c7143c893c6a76437ba2ac0eb5372bb39b8
valli8 <keevo5ni@t-online.de>
2016-12-11 18:25:18 +0100

    Rename classes

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,35 +3,35 @@
 if "bpy" in locals():
     # ...so we need to reload our submodule(s) using importlib
     import importlib
-    if "player_init" in locals():
-        importlib.reload(player_init)
-    if "car" in locals():
-        importlib.reload(car)
-    if "get_keys" in locals():
-        importlib.reload(get_keys)
-    if "get_steering" in locals():
-        importlib.reload(get_steering)
+    if "PlayerInit" in locals():
+        importlib.reload(PlayerInit)
+    if "Car" in locals():
+        importlib.reload(Car)
+    if "GetKeys" in locals():
+        importlib.reload(GetKeys)
+    if "GetSteering" in locals():
+        importlib.reload(GetSteering)
     if "bar" in locals():
         importlib.reload(bar)
-    if "get_mouse_move" in locals():
-        importlib.reload(get_mouse_move)
-    if "get_player_controls" in locals():
-        importlib.reload(get_player_controls)
+    if "GetMouseMove" in locals():
+        importlib.reload(GetMouseMove)
+    if "GetPlayerControls" in locals():
+        importlib.reload(GetPlayerControls)
     if "bump" in locals():
         importlib.reload(bump)
-    if "get_direction" in locals():
-        importlib.reload(get_direction)
+    if "GetDirection" in locals():
+        importlib.reload(GetDirection)
 
 
 # This is only relevant on first run, on later reloads those modules
 # are already in locals() and those statements do not do anything.
 import bpy
-from scripts.player_init import player_init
-from scripts.car import car
-from scripts.get_keys import get_keys
-from scripts.get_steering import get_steering
+from scripts.PlayerInit import PlayerInit
+from scripts.Car import Car
+from scripts.GetKeys import GetKeys
+from scripts.GetSteering import GetSteering
 from scripts.bar import bar
-from scripts.get_mouse_move import get_mouse_move
-from scripts.get_player_controls import get_player_controls
+from scripts.GetMouseMove import GetMouseMove
+from scripts.GetPlayerControls import GetPlayerControls
 from scripts.bump import bump
-from scripts.get_direction import get_direction
+from scripts.GetDirection import GetDirection
